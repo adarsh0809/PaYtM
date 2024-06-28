63ab87106cb7f9649af8726fc7722473982a5b85
adarsh0809 <99119770+adarsh0809@users.noreply.github.com>
2024-06-28 16:36:23 +0530

    backend code checkpoint :wq

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,17 @@
+// backend/index.js
 const express = require("express");
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
 
 
+const rootRouter = require("./routes/index");
+
+
+app.use("/api/v1", rootRouter);
+
+
+
+app.listen(3000);
